7c1febe8dc5a24741dd231d4f7092c0b25999897
Missael Aguilar <dev.missael@gmail.com>
2022-12-08 15:29:19 -0600

    Requirement – starting point and orientation (test and implementation)

--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -6,5 +6,12 @@ import static org.testng.Assert.*;
 
 @Test
 public class ShipSpec {
+    public void whenInstantiatedThenLocationIsSet() {
+        Point startingPoint = new Point(0, 0);
+        Direction startingDirection = Direction.NORTH;
+        Location startingLocation = new Location(startingPoint, startingDirection);
+        Ship ship = new Ship(startingLocation);
+        assertEquals(ship.getLocation(), startingLocation);
+    }
 
 }
